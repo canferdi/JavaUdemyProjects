3c333147165f41daac947373d51f00904c8fcc90
Ferdi Can <fcan5414@gmail.com>
2023-02-18 19:21:17 +0300

    switcher has been finished

---
+++ switcher.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class switcher {
+
+    // This project switches the first and second number received from the user
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int firstNum = scanner.nextInt();
+
+        System.out.println("Enter second number: ");
+        int secondNum = scanner.nextInt();
+
+        int temp = firstNum;
+        firstNum = secondNum;
+        secondNum = temp;
+
+        System.out.println("First number (switched): " + firstNum);
+        System.out.println("Second number (switched): " + secondNum);
+
+    }
+
+}
