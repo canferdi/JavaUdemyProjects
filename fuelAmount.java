c4920c3b0ac10580e5990d65db5919f84bfc1dc6
Ferdi Can <fcan5414@gmail.com>
2023-02-18 18:13:30 +0300

    fuelAmount project has been finished

---
+++ fuelAmount.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fuelAmount {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter fuel usage per kilometer (ex:0,32): ");
+        double fuelUsage = scanner.nextDouble();
+
+        System.out.println("Enter road taken: ");
+        int totalRoad = scanner.nextInt();
+
+        double result = fuelUsage * totalRoad;
+        System.out.println("Total fuel cost: " + result + "$");
+
+    }
+
+}
