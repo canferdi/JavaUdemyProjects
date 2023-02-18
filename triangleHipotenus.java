01c025fa429ae0e9afff72655ec61a765381c55f
Ferdi Can <fcan5414@gmail.com>
2023-02-18 19:48:30 +0300

    triangleHipotenus has been finished

---
+++ triangleHipotenus.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * triangleHipotenus
+ */
+public class triangleHipotenus {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first edge: ");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter second edge: ");
+        int b = scanner.nextInt();
+        
+        double hipotenus = Math.sqrt(a * a + b * b);
+        System.out.println("Hipotenus: " + hipotenus);
+    }
+}
