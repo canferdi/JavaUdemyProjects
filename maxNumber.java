7353c074e9ade1af89896af006c846f011fc9be4
Ferdi Can <fcan5414@gmail.com>
2023-02-21 21:58:46 +0300

    maxNumber project finished

---
+++ maxNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class maxNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter first number: ");
+        int firstNum = scanner.nextInt();
+        
+        System.out.println("Enter second number: ");
+        int secondNum = scanner.nextInt();
+
+        System.out.println("Enter third number: ");
+        int thirdNum = scanner.nextInt();
+
+        int maxNum = 0;
+
+        if (firstNum >= secondNum && firstNum >= thirdNum) {
+            maxNum = firstNum;
+        }
+        else if (secondNum >= firstNum && secondNum >= thirdNum) {
+            maxNum = secondNum;
+        }
+        else {
+            maxNum = thirdNum;
+        }
+        
+        System.out.println("Max number : " + maxNum);
+
+    }
+}
